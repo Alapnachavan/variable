a540c554b930d5ee5a9e023319ec035016d09f5c
Alapnachavan <90903743+Alapnachavan@users.noreply.github.com>
2021-10-05 01:14:34 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,10 @@
+ = input ("enter the day ") 
+time = ("enter the time ?dinner , lunch, breakfast")
+if day == "monday"
+time == "breakfast"
+print ("pohdaya")
+eilf time =="lunch" 
+print ("dal chaval") 
+time =="dinner" 
+print("dal subji")
+else : print ("invalid")
